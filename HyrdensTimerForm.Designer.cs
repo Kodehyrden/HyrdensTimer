3a96c5c182e9f93462f39854c3b213458360dd00
kodehyrde <niels.harre@gmail.com>
2015-12-05 10:41:27 +0100

    First commit

---
+++ HyrdensTimerForm.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace HyrdensTimer
+{
+    partial class HyrdensTimerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HyrdensTimerForm));
+            this.TimeLeft = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // TimeLeft
+            // 
+            this.TimeLeft.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TimeLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TimeLeft.Location = new System.Drawing.Point(0, 0);
+            this.TimeLeft.Name = "TimeLeft";
+            this.TimeLeft.Size = new System.Drawing.Size(632, 159);
+            this.TimeLeft.TabIndex = 0;
+            this.TimeLeft.Text = "-";
+            this.TimeLeft.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.TimeLeft.Click += new System.EventHandler(this.TimeLeft_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // HyrdensTimerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 159);
+            this.Controls.Add(this.TimeLeft);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "HyrdensTimerForm";
+            this.Text = "Hyrdens Timer";
+            this.Shown += new System.EventHandler(this.HyrdensTimerForm_Shown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label TimeLeft;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ HyrdensTimerForm.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HyrdensTimer
+{
+    public partial class HyrdensTimerForm : Form
+    {
+        private Point formLocation;
+        private System.Windows.Forms.FormBorderStyle borderStyle;
+        private System.Drawing.Size formSize;
+        private System.Drawing.Color txColor;
+        private System.Drawing.Color bgColor;
+        private bool fullScreen;
+        private int duration;
+        private DateTime startTime;
+        private DateTime endTime;
+
+        public HyrdensTimerForm()
+        {
+            InitializeComponent();
+            borderStyle = this.FormBorderStyle;
+            formSize = this.Size;
+            txColor = TimeLeft.ForeColor;
+            bgColor = this.BackColor;
+            fullScreen = false;
+            string[] args = Environment.GetCommandLineArgs();
+            try
+            {
+                // First argument is the time in minutes
+                duration = Convert.ToInt32(args[1]); 
+            }
+            catch (Exception)
+            {
+                // If the conversion goes wrong use as default 15 minutes
+                duration = 15;
+            }
+        }        
+        private void ToggleFullscreen()
+        {
+            if (!fullScreen)
+            {
+                formLocation = this.Location;
+                this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+                this.Bounds = Screen.FromControl(this).Bounds;
+                TimeLeft.ForeColor = System.Drawing.Color.White;
+                this.BackColor = System.Drawing.Color.Black;
+                fullScreen = true;
+            }
+            else
+            {
+                this.Location = formLocation;
+                this.FormBorderStyle = borderStyle;
+                this.Size = formSize;
+                TimeLeft.ForeColor = txColor;
+                this.BackColor = bgColor;
+                fullScreen = false;
+            }
+        }
+
+        private void UpdateLabel() 
+        {
+            TimeSpan timeLeft = endTime - DateTime.Now;
+            TimeLeft.Text = (timeLeft.TotalSeconds < 0 ? "-" : "") +
+                string.Format(Math.Abs(timeLeft.TotalMinutes) < 60 ? "{0:m\\:ss}" : "{0:h\\:mm\\:ss}", timeLeft);
+        }
+        
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            bool result = true;
+            switch (keyData)
+            {
+                case Keys.Escape:
+                    if (fullScreen) ToggleFullscreen(); // Exit fullscreen
+                    break;
+                case Keys.T:
+                    ToggleFullscreen();
+                    break;
+                default:
+                  result = base.ProcessCmdKey(ref msg, keyData);
+                  break;
+            }
+            return result;
+        }
+
+        private void HyrdensTimerForm_Shown(object sender, EventArgs e)
+        { 
+            startTime = DateTime.Now;
+            endTime = startTime + new TimeSpan(0, duration, 0); 
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            UpdateLabel();
+        }
+
+        private void TimeLeft_Click(object sender, EventArgs e)
+        {
+            ToggleFullscreen();
+        }
+    }
+}
